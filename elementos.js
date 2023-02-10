3a7dac4e48f1b8ac23436cab2d25e293f50457a1
Lkopinhos <121643799+Lkopinhos@users.noreply.github.com>
2023-02-10 18:14:51 -0300

    Initial commit

---
+++ elementos.js
@@ -0,0 +1,14 @@
+const Ca = {
+    Nome: "Calcio",
+    Numero_atomico: "20",
+    Config_eletronica: "[Ar] 4s²",
+    Familia: "Alcalino terrosos",
+    Nox:"+2"
+}
+const H = {
+    Nome: "Hidrogenio",
+    Numero_atomico: "1",
+    Config_eletronica: "1s1",
+    Familia: "Sem familia",
+    Nox: "1"
+}
